c4837db54c2acec6586f1540e94af6b1141a98d8
steven opio <stevenviko@gmail.com>
2021-08-19 20:22:56 +0300

    Refactor index.js file

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { Form, Select, Button } from 'semantic-ui-react'
-
+import { LoginAuthedUser } from '../actions/authedUser';
 
 
 class Login extends Component {
