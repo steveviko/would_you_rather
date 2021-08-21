c15287abb58a2e019924f751e2e962c39960602f
steven opio <stevenviko@gmail.com>
2021-08-21 13:16:29 +0300

    Add App demo link

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,10 +1,11 @@
 import { INIT_AUTHED_USER } from "../actions/authedUser";
 
 
-export default function authedUser(state = null, action) {
+export default function authedUser(state = null, action){
   switch (action.type) {
   case INIT_AUTHED_USER :
-    return action.id  
+    return action.id
+  
   default:
     return state
   }
